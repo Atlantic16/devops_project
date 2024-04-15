b3a060064bc5ab68d9e5207086d128ed65f2ceaa
baladia <ayoub.baladi@etu.univ-grenoble-alpes.fr>
2024-04-16 00:46:35 +0200

    [FEATURE]:DataFrame tuned according to DFelement; 2 Constructors added + add/pop/show functions

--- src/main/java/org/example/DataFrame.java
+++ src/main/java/org/example/DataFrame.java
@@ -3,9 +3,64 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Iterator;
+
+import java.io.FileReader;
+import java.io.Reader;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
 public class DataFrame {
-    List<DFelement> map = new ArrayList<>();
+    List<DFelement> map;
+
+    /**
+     * Constructor that take a DFelement array an populate the DataFrame with it.
+     * @param element
+     */
+    public DataFrame(DFelement[] element){
+        map = new ArrayList<>();
+
+        for(DFelement e: element){
+            if(e != null)
+                add(e);
+        }
+    }
+
+    /**
+     * Constructor that populate the DataFrame from a CSV file
+     * @param fileName a CSV file
+     */
+    public DataFrame(String fileName){
+        map = new ArrayList<>();
+        Reader reader;
+        CSVParser parser = null;
+
+        try{ //try parse file
+            reader = new FileReader(fileName);
+            parser = CSVFormat.DEFAULT.parse(reader);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        //get all records
+        List<CSVRecord> records = parser.getRecords();
+        String label;
+        List<Object> elems;
+        Iterator<?> it;
+
+        for(CSVRecord record: records){ //iterate over each record
+            it = record.iterator();
+            label = it.next().toString();
+            elems = new ArrayList<>();
+            while(it.hasNext()) //iterate over each element of a record
+                elems.add(it.next());
+            DFelement elem = new DFelement(label, elems.toArray()); //create an object DFelement
+            map.add(elem);
+        }
+
+    }
     
     /**
      * Add an element to DataFrame instance.
@@ -19,13 +74,13 @@ public class DataFrame {
 
     /**
      * Return the object of the key key, and drop it.
-     * @param key
+     * @param label
      * @return Object
      */
-    public Object pop(String key){
+    public Object pop(String label){
         Object res = null;
         for(DFelement obj: map){
-            if(obj.getKey() == key){
+            if(obj.getLabel() == label){
                 res = obj;
                 break;
             }
@@ -40,10 +95,10 @@ public class DataFrame {
      * @return void
      */
     public void print(DFelement elem){
-        String key = elem.getKey();
+        String label = elem.getLabel();
         Object value = elem.getValue();
         
-        System.out.print(key);
+        System.out.print(label);
         System.out.println(Arrays.toString((Object[]) value));
     }
 
