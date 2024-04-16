738605f991672d5c833fd2ed233217c1d8c28d9d
nizirux <azznizar9@gmail.com>
2024-04-16 09:32:21 +0200

    [TEST] DataFrameTest added (not complete to be discussed)

---
+++ src/test/java/org/example/DataFrameTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+public class DataFrameTest {
+
+    private DataFrame dataframe;
+    private DFelement[] elements;
+    @Before
+    public void setUp(){
+
+    }
+    @Test
+    public void constructorTest(){
+
+    }
+
+    @Test
+    public void addTest(){
+
+    }
+
+    @Test
+    public void popTest(){
+
+    }
+
+    @Test
+    public void getRowTest(){
+
+    }
+
+    @Test
+    public void printTest(){
+
+    }
+}
